7329744ad29652ad4824106aa1c76b8973f56f26
Hidden-s574 <talhashaad784480@gmail.com>
2026-04-07 22:23:58 +0530

    Add hollow diamond pattern printing functionality
    Implemented a method to print a hollow diamond pattern of half-height n using '*' characters.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,25 +1,31 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
-
+        for (int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n - i; j++) sb.append(' ');
+            if (i == 1) {
+                sb.append('*');
+            } else {
+                sb.append('*');
+                for (int j = 0; j < 2 * i - 3; j++) sb.append(' ');
+                sb.append('*');
+            }
+            System.out.println(sb);
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n - i; j++) sb.append(' ');
+            if (i == 1) {
+                sb.append('*');
+            } else {
+                sb.append('*');
+                for (int j = 0; j < 2 * i - 3; j++) sb.append(' ');
+                sb.append('*');
+            }
+            System.out.println(sb);
+        }
     }
 }
