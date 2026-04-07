4e5e849c02c92492ca5cb3214549d15f0fbc5abe
Hidden-s574 <talhashaad784480@gmail.com>
2026-04-07 22:27:09 +0530

    Fix duplicate row update in Main.java
    Removed duplicate row update in the loop.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -6,13 +6,13 @@ public class Main {
         long[] row = new long[n];
         row[0] = 1;
         for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 1; j--) row[j] += row[j - 1];
             StringBuilder sb = new StringBuilder();
             for (int j = 0; j <= i; j++) {
                 if (j > 0) sb.append(' ');
                 sb.append(row[j]);
             }
             System.out.println(sb);
-            for (int j = i; j >= 1; j--) row[j] += row[j - 1];
         }
     }
 }
